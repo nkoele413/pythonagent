8095bd325904961e59fe41acbaeaa40635e2fa9e
nkoele413 <nkoele413@gmail.com>
2025-06-23 21:14:47 -0600

    "update to main"

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model = "gemini-2.0-flash-001"
+        model = "gemini-2.0-flash-001",
         content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
