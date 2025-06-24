ff4233fd383e0d1d1ca275617f430924823af965
nkoele413 <nkoele413@gmail.com>
2025-06-23 21:38:16 -0600

    updated main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 import os
-from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from dotenv import load_dotenv
 
 def main():
     load_dotenv()
@@ -17,12 +18,22 @@ def main():
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+def generate_content(client,messages):
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001",
-        contents = user_prompt,
+        contents = messages,
+        
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
     print("Response:")
     print(response.text)
 
