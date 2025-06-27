030cef5df92e730014df2e2ab97f49da7247f997
nkoele413 <nkoele413@gmail.com>
2025-06-26 21:35:53 -0600

    fix to main config

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ def generate_content(client,messages, verbose):
     response = client.models.generate_content(
         model = "gemini-2.0-flash-001",
         contents = messages,
-        config = types.GenerateContentConfig(system_instruction = system_prompt),
         config=types.GenerateContentConfig(
             tools=[available_functions], system_instruction=system_prompt),
     )
