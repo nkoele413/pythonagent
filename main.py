0ca2d60cb03f3b3623124cc9443b43d4675ae5f1
nkoele413 <nkoele413@gmail.com>
2025-06-26 21:34:35 -0600

    improved promting

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 import os
 from google import genai
-from google.genai import types
-from dotenv import load_dotenv
+from google.genai import types # type: ignore
+from dotenv import load_dotenv # type: ignore
 from prompts import system_prompt
+from call_function import available_functions
 
 def main():
     load_dotenv()
@@ -37,12 +38,17 @@ def generate_content(client,messages, verbose):
         model = "gemini-2.0-flash-001",
         contents = messages,
         config = types.GenerateContentConfig(system_instruction = system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)    
-    print("Response:")
-    print(response.text)
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 if __name__ == "__main__":
     main()
